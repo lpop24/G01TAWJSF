fc7c98fe558d60d047e5b7b542d6b1aad6b1bbc5
lpop24 <Usuario@DESKTOP-4A75LLF>
2017-05-25 06:00:49 +0200

    Nuevo proyecto. Entidades y Fachadas. JSF en modulo war

---
+++ RedSocialG01JSF/RedSocialG01JSF-war/src/java/managedBeans/CrearUsuarioMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Loubna Channouf Cherradi
+ */
+@Named(value = "crearUsuarioMB")
+@RequestScoped
+public class CrearUsuarioMB {
+
+    /**
+     * Creates a new instance of CrearUsuarioMB
+     */
+    public CrearUsuarioMB() {
+    }
+    
+}
