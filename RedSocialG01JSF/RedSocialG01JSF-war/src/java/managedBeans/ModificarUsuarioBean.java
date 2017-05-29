7d4171bef43a986edfdbf8d9c50bf997169f5fd1
alvar <alvar@DESKTOP-7BINTOU>
2017-05-29 18:29:06 +0200

    primeras modificaciones

--- RedSocialG01JSF/RedSocialG01JSF-war/src/java/managedBeans/ModificarUsuarioBean.java
+++ RedSocialG01JSF/RedSocialG01JSF-war/src/java/managedBeans/ModificarUsuarioBean.java
@@ -5,21 +5,55 @@
  */
 package managedBeans;
 
+import entity.Usuario;
+import facade.UsuarioFacade;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 /**
  *
  * @author Usuario
  */
-@Named(value = "modificarUsuarioMB")
+@Named
 @RequestScoped
 public class ModificarUsuarioBean {
+    
+    @EJB
+    private UsuarioFacade usuarioFacade;
 
+    @Inject LoginBean loginBean;
+    
+    protected Usuario usuario;
     /**
      * Creates a new instance of ModificarUsuarioMB
      */
     public ModificarUsuarioBean() {
+    }
+    
+    @PostConstruct
+    public void init()
+    {
+        if(loginBean.getUsuarioSeleccionado() == null)
+            usuario = new Usuario();
+        else
+            usuario = loginBean.getUsuarioSeleccionado();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public String doGuardar()
+    {
+        usuarioFacade.edit(usuario);
+        return "perfil";
     }
     
 }
